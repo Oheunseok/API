cdc56cd142a9b926dfe82204125090f761a1e249
wonsub <secretfice@naver.com>
2016-05-14 02:41:54 +0900

    파싱완료
    순회하면서 각 태그마다 출력

--- xtml.py
+++ xtml.py
@@ -30,19 +30,54 @@ def objects_by_id(id_):
             return obj
     raise Exception("No found")
 
-tree=ET.parse("MovieList.xml")
+
+url=urllib.request.urlopen("http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101")
+tree=ET.parse(url)
+#tree=ET.parse("MovieList.xml")
 note=tree.getroot()
 
-print (note.findtext("boxofficeType"))
+
 List(tree)
 
 indent(note)
-ET.dump(note)
+#ET.dump(note)
+
+print(note.findtext("boxofficeType"))
+print(note.findtext("showRange"))
+for element in note.findall("dailyBoxOfficeList"):
+    for tag in element.findall("dailyBoxOffice"):
+        print("rnum ",tag.findtext("rnum"))
+        print("rank ",tag.findtext("rank"))
+        print("rankInten",tag.findtext("rankInten"))
+        print("rankOldAndNew",tag.findtext("rankOldAndNew"))
+        print("movieCd", tag.findtext("movieCd"))
+        print("movieNm", tag.findtext("movieNm"))
+        print("openDt", tag.findtext("openDt"))
+        print("salesAmt", tag.findtext("salesAmt"))
+        print("salesShare", tag.findtext("salesShare"))
+        print("salesInten", tag.findtext("salesInten"))
+        print("salesChange", tag.findtext("salesChange"))
+        print("salesAcc", tag.findtext("salesAcc"))
+        print("audiCnt", tag.findtext("audiCntaudiCnt"))
+        print("audiInten", tag.findtext("audiInten"))
+        print("audiChange", tag.findtext("audiChange"))
+        print("salesAcc", tag.findtext("salesAcc"))
+        print("scrnCnt", tag.findtext("scrnCnt"))
+        print("showCnt", tag.findtext("showCnt"))
 
 
-for parent in tree.getiterator():
-    for child in parent:
-        print(child)
+
+
+
+
+
+        print()
+
+
+
+#for parent in tree.getiterator():
+#    for child in parent:
+#        print(child)
 
 
 
